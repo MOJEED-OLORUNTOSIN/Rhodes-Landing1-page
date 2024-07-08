df0beead2d323f759fddd439d9fe9b910ae1385a
MOJEED-OLORUNTOSIN <mojeedoloruntosin@gmail.com>
2024-07-08 22:59:43 +0100

    furst commit

---
+++ Assets/js/app.js
@@ -0,0 +1,27 @@
+const navbar = document.querySelector(".navbar");
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const navLinksLi = document.querySelector(".nav-links li");
+window.addEventListener("scroll", () => {
+  if (this.scrollY >= 100) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+  hamburger.classList.toggle("active");
+});
+
+navLinksLi.forEach((li) =>
+  li.addEventListener("click", () => {
+    navLinksLi.forEach((li) => li.classList.remove("active"));
+    li.classList.add("active");
+
+    hamburger.classList.remove(active);
+    navLinks.classList.remove("active");
+  })
+);
+
+// AOS
