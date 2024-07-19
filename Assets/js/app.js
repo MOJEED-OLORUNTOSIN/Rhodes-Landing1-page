faa201c14820b53a59abfa1071ac32839b9c6359
MOJEED-OLORUNTOSIN <mojeedoloruntosin@gmail.com>
2024-07-19 04:25:26 +0100

    loader,tag, navbar upgrade

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -2,6 +2,7 @@ const navbar = document.querySelector(".navbar");
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const navLinksLi = document.querySelector(".nav-links li");
+
 window.addEventListener("scroll", () => {
   if (this.scrollY >= 100) {
     navbar.classList.add("scrolled");
