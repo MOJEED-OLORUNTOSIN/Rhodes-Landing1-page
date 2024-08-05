d316b4bd7670dd5326197c12cef846e9cae7bc49
MOJEED-OLORUNTOSIN <mojeedoloruntosin@gmail.com>
2024-08-05 02:44:09 +0100

    Javascript update

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,35 +1,30 @@
-const navbar = document.querySelector(".navbar");
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
-const navLinksLi = document.querySelector(".nav-links li");
-
-window.addEventListener("scroll", () => {
-  if (this.scrollY >= 100) {
-    navbar.classList.add("scrolled");
-  } else {
-    navbar.classList.remove("scrolled");
-  }
-});
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-  hamburger.classList.toggle("active");
-});
-
-navLinksLi.forEach((li) =>
-  li.addEventListener("click", () => {
-    navLinksLi.forEach((li) => li.classList.remove("active"));
-    li.classList.add("active");
-
-    hamburger.classList.remove(active);
-    navLinks.classList.remove("active");
-  })
-);
-
 document.addEventListener("DOMContentLoaded", () => {
-  navLinks.forEach((li) => {
+  const navbar = document.querySelector(".navbar");
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
+  const navLinksItems = document.querySelectorAll(".nav-links li");
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 100) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  });
+  hamburger.addEventListener("click", () => {
+    if (navLinks.classList.contains("active")) {
+      navLinks.classList.remove("active");
+      hamburger.classList.remove("active");
+    } else {
+      navLinks.classList.add("active");
+      hamburger.classList.add("active");
+    }
+  });
+  navLinksItems.forEach((li) => {
     li.addEventListener("click", () => {
-      navLinks.add("hidden");
+      if (navLinks.classList.contains("active")) {
+        navLinks.classList.remove("active");
+        hamburger.classList.remove("active");
+      }
     });
   });
 });
-// AOS
